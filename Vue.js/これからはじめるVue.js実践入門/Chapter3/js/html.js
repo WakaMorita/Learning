9d6b2e7bde42ae957439f545820d0fa8ab0c7123
waka <waka_morita_business@yahoo.co.jp>
2022-12-15 23:12:30 +0900

    Chapter3-4

---
+++ Vue.js/これからはじめるVue.js実践入門/Chapter3/js/html.js
@@ -0,0 +1,8 @@
+new Vue({
+    el: '#app',
+    data: {
+        message: `<h3>WINGSプロジェクト</h3>
+        <img src="https://www.web-deli.com/image/linkbanner_l.gif" alt="ロゴ" />`
+        // ``は行にまたがる文字列
+    }
+})
