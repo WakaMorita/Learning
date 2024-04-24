2e4c5175f0772b9ca7205f516cfc89cc8250d96f
waka <waka_morita_business@yahoo.co.jp>
2024-04-24 22:39:40 +0900

    ✨chapter2_1

---
+++ Nuxt3/NuxtLearning/01/components-basic/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <sectioon>
+    <h1>コンポーネント基礎</h1>
+    <!-- 以下はコンポーネント -->
+    <OneSection/>
+    <OneSection/>
+    <PartsTheSupplement/>
+  </sectioon>
+</template>
+
+<style scoped>
+section {
+  border: blue 1px solid;
+  margin: 10px
+}
+</style>
---
+++ Nuxt3/NuxtLearning/01/components-basic/components/OneSection.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="box">
+        <h4>ひとつのコンポーネント</h4>
+        <p>
+            コンポーネントとは、・・・・
+        </p>
+    </section>
+</template>
+
+<style scoped>
+.box {
+    border: green 1px dashed;
+    margin: 10px;
+}
+</style>
+
+<!-- 
+    section
+    https://developer.mozilla.org/ja/docs/Web/HTML/Element/section
+    class
+    https://developer.mozilla.org/ja/docs/Web/HTML/Global_attributes/class
+    template
+    https://developer.mozilla.org/ja/docs/Web/HTML/Element/template
+    scoped
+    https://vue-loader-v14.vuejs.org/ja/features/scoped-css.html
+    dashed
+    https://www.htmq.com/style/border-style.shtml
+ -->
